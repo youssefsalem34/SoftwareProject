ff10301b3e5287fd2fc9fe329eade9456ace9750
Yousifismael <youssef.ismail0111@gmail.com>
2025-04-17 15:52:26 +0100

    fea

---
+++ Assets/Scripts/Player1/Objectives/CollectingObjects/Timer.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private PlayerDeath deathScript;
+    [SerializeField] private float time;
+    [SerializeField] private float resetTimer;
+    [SerializeField] private TMP_Text timerDisplay;
+    [SerializeField] private GameObject secondObj;
+    [SerializeField] private bool timerDone;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        //Timerr();
+
+        if (secondObj.gameObject.activeSelf)
+        {
+            time = time - Time.deltaTime;
+            UpdateTimerDisplay();
+        }
+        //else
+        //{
+        //    time = resetTimer;
+        //}
+
+
+        if (time <= 0)
+        {
+            timerDone = true;
+        }
+
+        if (timerDone)
+        {
+            deathScript.playerDead = true;
+            time = resetTimer;
+            timerDone = false;
+        }
+    }
+
+
+
+    void Timerr()
+    {
+        
+    }
+
+
+    private void UpdateTimerDisplay()
+    {
+        int minutes = Mathf.FloorToInt(time / 60); // Calculate minutes
+        int seconds = Mathf.FloorToInt(time % 60); // Calculate seconds
+        timerDisplay.text = string.Format("{0:00}:{1:00}", minutes, seconds); // Display in "MM:SS" format
+    }
+}
